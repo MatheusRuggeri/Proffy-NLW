f5005992379ab1c3f94b0ba035b53f24fcec9637
MatheusRuggeri <jomarumu@yahoo.com.br>
2020-08-07 15:31:08 -0300

    Aula 3 - Até 57m30s
    Antes do conceito de estado

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
 import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
@@ -20,13 +22,52 @@ function TeacherForm() {
                     <Input name="name" label="Nome Completo" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
                 </fieldset>
 
                 <fieldset>
                     <legend>Sobre a aula</legend>
                     
-                    <Input name="subject" label="Matéria" />
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            {value: 'Artes', label: 'Artes'},
+                            {value: 'Biologia', label: 'Biologia'},
+                            {value: 'Ciências', label: 'Ciências'},
+                            {value: 'Educação física', label: 'Educação física'},
+                            {value: 'Física', label: 'Física'},
+                            {value: 'Geografia', label: 'Geografia'},
+                            {value: 'História', label: 'História'},
+                            {value: 'Matemática', label: 'Matemática'},
+                            {value: 'Português', label: 'Português'},
+                            {value: 'Química', label: 'Química'},
+                        ]}
+                    />
                     <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Horários disponíveis
+                        <button type="button">+ Novo horário</button>
+                    </legend>
+                    <div className="schedule-item">
+                        <Select 
+                            name="week_day" 
+                            label="Dia da Semana"
+                            options={[
+                                {value: '0', label: 'Domingo'},
+                                {value: '1', label: 'Segunda'},
+                                {value: '2', label: 'Terça'},
+                                {value: '3', label: 'Quarta'},
+                                {value: '4', label: 'Quinta'},
+                                {value: '5', label: 'Sexta'},
+                                {value: '6', label: 'Sábado'},
+                            ]}
+                        />
+                        <Input name="from" label="Das" type="time" />
+                        <Input name="to" label="Até" type="time" />
+                    </div>
                 </fieldset>
 
                 <footer>
