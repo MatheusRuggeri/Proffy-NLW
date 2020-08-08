a79a4ffe085a98c0b15ad1ca9ba84d735080e010
MatheusRuggeri <jomarumu@yahoo.com.br>
2020-08-08 19:39:23 -0300

    Aula 4 em 1h28m
    Funcionando muito mal, as fontes não carregam e ninguém sabe o porque.
    O app não funciona direito.

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, Text, TextInput } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons'
+
+import styles from './styles'
+
+function Favorites() {
+    return (
+        <View/>
+    );
+}
+
+export default Favorites;
---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, Text, TextInput } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons'
+
+import styles from './styles'
+
+function TeacherList() {
+    return (
+        <View/>
+    );
+}
+
+export default TeacherList;
